305d62a3df8ab0a2facad6df933ce9423ff28323
xusheng <klein203@hotmail.com>
2019-12-16 22:08:08 +0800

    rebase,

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path(r'', views.index, name='index'),
-    # path(r'^(?P<blog_id>[0-9]+)', views.detail, name='detail'),
+    path('<int:blog_id>/detail', views.detail, name='detail'),
 ]
